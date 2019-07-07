65e9dc0aee8836aeca3320005a84027d5497306d
nam2934 <nam2934@naver.com>
2019-07-08 02:03:40 +0900

    view

---
+++ view_controller/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root "home#index"
+    #get '/'=> 'home#index'
+
+    get '/attack', to: 'home#attack'
+    get '/defense', to: 'home#defense'
+end
