57d636548bf7acef0310bcd68c341a8e4e948179
nam2934 <nam2934@naver.com>
2019-07-09 22:06:39 +0900

    add post and delete

--- note_project/config/routes.rb
+++ note_project/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root "home#index"
+
+    get 'home/index'
+
+    get 'home/new'
+
+    post 'home/create'
+
+    get 'home/destroy/:post_id' => 'home#destroy'
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
